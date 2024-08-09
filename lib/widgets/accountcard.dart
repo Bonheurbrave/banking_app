52dbe7a7a2c9bf6f93e6ffd8a60428a25bad862d
Bonheurbrave <bonheurbrave1@gmail.com>
2024-08-09 22:10:33 +0200

    saved:

---
+++ lib/widgets/accountcard.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Accountcard extends StatefulWidget {
+  const Accountcard({super.key});
+
+  @override
+  State<Accountcard> createState() => _AccountcardState();
+}
+
+class _AccountcardState extends State<Accountcard> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
