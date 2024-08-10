a0848e8b8413ee9c0920c807ac3f3424bd3e3197
Bonheurbrave <bonheurbrave1@gmail.com>
2024-08-10 19:15:22 +0200

    saved

--- lib/pages/profilepage.dart
+++ lib/pages/profilepage.dart
@@ -5,7 +5,17 @@ class Profilepage extends StatelessWidget {
   build (BuildContext context){
     return Scaffold(
       appBar: AppBar(title: Text("Profile page"),),
-      body: Center(child:Text("centered text"))
+      body: Center(child:Column(children: [
+        CircleAvatar(
+          child: Stack(
+            children: [
+              Container(child: 
+              //profile picture
+              Image.asset("assetimages"),)
+            ],
+          ),
+        )
+      ],))
       
     );
   }
