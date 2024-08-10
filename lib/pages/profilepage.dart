10a589769afbefff1856a58c7eac48750841cb60
Bonheurbrave <bonheurbrave1@gmail.com>
2024-08-10 19:13:20 +0200

    saved

---
+++ lib/pages/profilepage.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Profilepage extends StatelessWidget {
+  @override
+  build (BuildContext context){
+    return Scaffold(
+      appBar: AppBar(title: Text("Profile page"),),
+      body: Center(child:Text("centered text"))
+      
+    );
+  }
+}
